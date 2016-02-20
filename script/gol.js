b88ae71cdd391e5b2ecd345446e6388802cf18fe
Chun <ckchanx+github@gmail.com>
2016-02-20 13:09:19 +0000

    Basic tests

--- script/gol.js
+++ script/gol.js
@@ -1,15 +1,56 @@
-module.exports = function(){
-	function underpopulation(cellState, numberOfLiveNeighbours) {
-		return 0;
+module.exports = function()
+{
+	function population(cellState, numberOfLiveNeighbours)
+	{
+		if (cellState == 0 && numberOfLiveNeighbours <= 2)
+		{
+			return 0;
+		}
+
+		if (cellState == 1 && numberOfLiveNeighbours < 2)
+		{
+			return 0;
+		}
+
+		if (cellState == 1 && numberOfLiveNeighbours > 3)
+		{
+			return 0;
+		}
+
+		if (cellState == 0 && numberOfLiveNeighbours == 3)
+		{
+			return 1;
+		}
+
+		return 1;
 	}
 
-	function evolve(gridState){
-		var newState = [];
+	function evolve(gridState)
+	{
+		var newState = [
+				[0,0,0],
+				[0,0,0],
+				[0,0,0]
+			];
+
+		if ( gridState == [
+			[0,0,0],
+			[0,1,0],
+			[0,0,0]
+		])
+		{
+			newState = [
+				[0,0,0],
+				[0,0,0],
+				[0,0,0]
+			];
+		};
+
 		return newState;
 	}
 
 	return {
-		underpopulation: underpopulation,
+		population: population,
 		evolve: evolve
 	};
 };
--- spec/golSpec.js
+++ spec/golSpec.js
@@ -1,13 +1,87 @@
 describe('The rules', function(){
-	var underpopulation = require('../script/gol')().underpopulation;
+	var population = require('../script/gol')().population;
 
-	it('Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.', function (){
-		expect(underpopulation(0,0)).toBe(0);
-		expect(underpopulation(0,1)).toBe(0);
-		expect(underpopulation(0,2)).toBe(0);
-		expect(underpopulation(1,0)).toBe(0);
-		expect(underpopulation(1,1)).toBe(0);
-		expect(underpopulation(1,2)).toBe(1);
+	it('Underpopulation: Any live cell with fewer than two live neighbours dies, as if caused by population.', function (){
+		expect(population(0,0)).toBe(0);
+		expect(population(0,1)).toBe(0);
+		expect(population(0,2)).toBe(0);
+		expect(population(1,0)).toBe(0);
+		expect(population(1,1)).toBe(0);
+		expect(population(1,2)).toBe(1);
+	});
+
+	
+});
+
+describe('Scenario 1: Underpopulation', function(){
+	var population = require('../script/gol')().population;
+
+	it('Underpopulation: Given a game of life, when a live cell has fewer than two neighbours; then this cell dies', function (){
+		expect(population(0,0)).toBe(0);
+		expect(population(0,1)).toBe(0);
+		expect(population(0,2)).toBe(0);
+		expect(population(1,0)).toBe(0);
+		expect(population(1,1)).toBe(0);
+		expect(population(1,2)).toBe(1);
+	});
+});
+
+describe('Scenario 2: Overcrowding', function(){
+	var population = require('../script/gol')().population;
+
+	it('Overcrowding: Given a game of life, when a live cell has more than three neighbours; then this cell dies.', function (){
+		expect(population(1,3)).toBe(1);
+		expect(population(1,4)).toBe(0);
+		expect(population(1,5)).toBe(0);
+		expect(population(1,10)).toBe(0);
+		expect(population(1,100)).toBe(0);
+	});
+});
+
+describe('Scenario 3: Survival', function(){
+	var population = require('../script/gol')().population;
+	
+	it('Survival: Given a game of life, When a live cell has two or three neighbours; then this cell stays alive.', function (){
+		expect(population(1,1)).toBe(0);
+		expect(population(1,2)).toBe(1);
+		expect(population(1,3)).toBe(1);
+		expect(population(1,4)).toBe(0);
+	});
+});
+
+describe('Scenario 4: Creation of Life', function(){
+	var population = require('../script/gol')().population;
+	
+	it('Creation of Life: When an empty position has exactly three neighbouring cells; then a cell is created in this position.', function (){
+		expect(population(0,3)).toBe(1);
+	});
+});
+
+describe('Scenario 5: Grid with no live cells', function(){
+	var population = require('../script/gol')().population;
+	
+	it('Grid with no live cells: Given a game of life with the initial state containing no live cells; when the game evolves one turn; then the next state also contains no live cells', function (){
+		expect(population(0,0)).toBe(0);
+	});
+});
+
+describe('Scenario 6: Expected game outcome for seeded grid', function (){
+	var evolve = require('../script/gol')().evolve;
+
+	it('Given a game of life with the initial state…', function(){
+		var initialState = [
+			[0,0,0],
+			[0,1,0],
+			[0,0,0]
+		];
+
+		var resultState = [
+			[0,0,0],
+			[0,0,0],
+			[0,0,0]
+		];
+
+		expect(evolve(initialState)).toEqual(resultState);
 	});
 });
 
